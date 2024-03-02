c544ad18788f371bc2e3f0012eb7f5875b89235a
Tihh Gonçalves <tihhgoncalves@gmail.com>
2024-03-02 18:12:53 -0300

    Instalando plugins

--- load.js
+++ load.js
@@ -1,11 +1,21 @@
 import * as components from './components/index.js';
+import * as plugins from './plugins/index.js';
 import routes from './routes.js';
 import store from './store';
 
+/** Instalador */
 const install = (app) => {
+  
+  //Importa Plugins
+  Object.entries(plugins).forEach(([pluginName, plugin]) => {
+    plugin(app);
+  });
+
+  //Importa Componentes
   Object.entries(components).forEach(([componentName, component]) => {
     app.component(componentName, component);
   });
+
 };
 
 // Exportações nomeadas
--- pages/teste.vue
+++ pages/teste.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
         [PÁGINA DE TESTE]
+        {{ $sdk }}
     </div>
 </template>
+
+<script>
+export default {
+    mounted () {
+        console.log('TESTE', this.$sdk);
+
+        this.$bus.$on('teste', () => {
+            this.$swal('Hello Vue world!!!');
+        })
+
+        this.$bus.$emit('teste');
+        
+    },
+}
+</script>
